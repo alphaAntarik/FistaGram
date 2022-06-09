53d8580f515b60395796f57375cdd95170693f59
alphaAntarik <antarikpanditison@gmail.com>
2022-06-09 12:34:17 +0530

    This is a login project

--- FistaGram2/app/src/main/java/com/example/fistagram/MainActivity.kt
+++ FistaGram2/app/src/main/java/com/example/fistagram/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.fistagram
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -16,6 +23,7 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var navController:NavController
     lateinit var BottomNavigationView:BottomNavigationView
+    val permissions = arrayOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,6 +38,16 @@ class MainActivity : AppCompatActivity() {
         BottomNavigationView.setupWithNavController(navController)
 
     }
+
+    fun cameraAccess(view: View) {
+        val intent = Intent(this,cameraActivity::class.java)
+
+
+            startActivity(intent)
+
+    }
+
+
 //    val onNevigationItemSelectedListener = BottomNavigationView.setOnNavigationItemSelectedListener {
 //        when(item.itemid()){
 //            R.id.home->
--- FistaGram2/app/src/main/java/com/example/fistagram/homeFragment.kt
+++ FistaGram2/app/src/main/java/com/example/fistagram/homeFragment.kt
@@ -13,6 +13,7 @@ class homeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
     }
 
     override fun onCreateView(
